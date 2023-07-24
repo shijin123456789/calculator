a415d242283541e1c59c8d5da5c1491b34b6cdb0
Shijin <096shijin@gmail.com>
2023-07-24 12:06:43 +0530

    master:v1

---
+++ calc.py
@@ -0,0 +1,29 @@
+
+from add_num import add
+
+from sub_num import subtract
+print("Calculator Program")
+
+while True:
+    print("\nSelect operation:")
+    print1. Add")
+    print("2. Subtract")
+    print("3. Exit")
+
+    choice = input("Enter your choice (1-3): ")
+
+    if choice == '3':
+        print("Exiting the program...")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid choice. Please try again.")
